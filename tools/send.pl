73a7088028882463b487b4d37d23e3250578eb19
Rubin <rubin@afternet.org>
2014-09-13 22:54:00 -0700

    dont need most of this code now, moved to the plugin

--- tools/send.pl
+++ tools/send.pl
@@ -11,33 +11,33 @@ my $msg = $ARGV[0];
 #Remove timestamp and [Server] lines
 $msg =~ s/^\[[0-9:]+\] \[[^]]+\]: //;
 
-#Someone speaking
-if( $msg =~ /^(\<.+\> .+)/ ) {
-    $msg = $1;
-}
-elsif($msg =~ /^(\w+) joined the game/) {
-    $msg =  "$1 connected";
-}
-elsif($msg =~ /^(\w+) left the game/) {
-    $msg =  "$1 left";
-}
-elsif($msg =~ /^(\w+ has just earned the achievement.*)/) {
-    $msg =  "-$1";
-}
-elsif($msg =~ /^(\w+ was slain by .*)/) {
-    $msg =  "-$1";
-}
-elsif($msg =~ /^\[(Server|Rcon)\] (.+)/) {
-    $msg =  ": $2";
-}
-#[00:08:37] [Server thread/INFO]: peaceenz suffocated in a wall
-
-else {
-#    print "                 DEBUG: skipping '$msg'\n";
-    exit 0; #skip this line
-}
-
-$msg = "[simplanet] $msg";
+##Someone speaking
+#if( $msg =~ /^(\<.+\> .+)/ ) {
+#    $msg = $1;
+#}
+#elsif($msg =~ /^(\w+) joined the game/) {
+#    $msg =  "$1 connected";
+#}
+#elsif($msg =~ /^(\w+) left the game/) {
+#    $msg =  "$1 left";
+#}
+#elsif($msg =~ /^(\w+ has just earned the achievement.*)/) {
+#    $msg =  "-$1";
+#}
+#elsif($msg =~ /^(\w+ was slain by .*)/) {
+#    $msg =  "-$1";
+#}
+#elsif($msg =~ /^\[(Server|Rcon)\] (.+)/) {
+#    $msg =  ": $2";
+#}
+##[00:08:37] [Server thread/INFO]: peaceenz suffocated in a wall
+#
+#else {
+##    print "                 DEBUG: skipping '$msg'\n";
+#    exit 0; #skip this line
+#}
+#
+#$msg = "[simplanet] $msg";
 
 #$msg =~ s/\'/\\\\\'/;
 #print "DEBUG: $msg\n";
